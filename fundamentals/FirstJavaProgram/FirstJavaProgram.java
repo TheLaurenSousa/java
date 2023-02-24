e05e137d3d33aa0a1031b5544350d1dfd1f7c67c
Lauren Sousa <thelaurensousa@gmail.com>
2023-02-24 14:35:37 -0500

    first program

---
+++ fundamentals/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Lauren Sousa");
+        System.out.println("I am 1,000 years old");
+        System.out.println("I live in Rochester, NY");
+    }
+}
