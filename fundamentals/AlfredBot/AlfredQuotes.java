1a63f0ce0b0fa686ec18b718246970a7cc07b361
Lauren Sousa <thelaurensousa@gmail.com>
2023-02-25 12:51:18 -0500

    AlfredBot

---
+++ fundamentals/AlfredBot/AlfredQuotes.java
@@ -0,0 +1,31 @@
+import java.util.Date;
+public class AlfredQuotes {
+    
+    public String basicGreeting() {
+        return "Hello, lovely to see you. How are you?";
+    }
+    
+    public String guestGreeting(String name) {
+        return "Greetings " + name + ". Lovely to see you.";
+    }
+
+    public String guestGreeting(String name, String dayPeriod){
+        return "Good " + dayPeriod + ", " + name + ". Lovely to see you.";
+    }
+    
+    public String dateAnnouncement() {
+        Date date = new Date();
+        return "It is currently " + date;
+    }
+    
+    public String respondBeforeAlexis(String conversation) {
+        if (conversation.contains("Alexis")){
+            return "Right away, sir. She certainly isn't sophisticated enough for that.";
+        } else if (conversation.contains("Alfred")){
+            return "At your service. As you wish, naturally.";
+        } else {
+            return "Right. And with that I shall retire.";
+        }
+    }
+}
+
